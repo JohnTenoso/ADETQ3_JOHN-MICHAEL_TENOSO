039c18211dbfba967af056cd872311f145dd3325
John Teñoso <Johntenoso1@gmail.com>
2020-10-13 17:46:49 +0800

    Commit a message

---
+++ ADETQ3_JOHN-MICHAEL_TENOSO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_JOHN_MICHAEL_TENOSO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
